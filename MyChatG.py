8e9e401e3ea2709f2d1b3b5f3f86ecdd1d4ecc35
calicuss <106090169+calicuss@users.noreply.github.com>
2023-06-12 21:05:00 +0200

    Add files via upload

---
+++ MyChatG.py
@@ -0,0 +1,22 @@
+import openai
+import gradio
+
+openai.api_key= "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"
+
+messages = [{"role": "system", "content": "Sei uno psicologo"}]
+
+def CustomChatGPT(user_input):
+    messages.append({"role": "system", "content": user_input})
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = messages
+    )
+    ChatGPT_reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "system", "content": ChatGPT_reply})
+    return ChatGPT_reply
+
+demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "PSICOLOGO DIGITALE")
+demo.launch(share= True)
+
+
+
